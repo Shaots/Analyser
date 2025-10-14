91262d389629acff0b02fc1e76c2337cf442094b
Roman <roman.miv@yandex.ru>
2025-10-14 14:54:15 +0300

    [u] Finish IMetric

--- src/metric.cpp
+++ src/metric.cpp
@@ -22,13 +22,17 @@
 
 namespace analyser::metric {
 
-void MetricExtractor::RegisterMetric(std::unique_ptr<IMetric> metric) {
-    metrics.emplace_back(std::move(metric));
-}
+void MetricExtractor::RegisterMetric(std::unique_ptr<IMetric> metric) { metrics.emplace_back(std::move(metric)); }
 
+// В 24.04 не поддерживается std::ranges::to<>()
 MetricResults MetricExtractor::Get(const function::Function &func) const {
-    // здесь ваш код
-    return {};
+    auto res = metrics | std::views::transform([&func](auto &&metric) { return metric->Calculate(func); });
+    MetricResults resVec;
+    resVec.reserve(std::ranges::distance(res));
+    for (auto ele : res) {
+        resVec.emplace_back(std::move(ele));
+    }
+    return resVec;
 }
 
 }  // namespace analyser::metric
