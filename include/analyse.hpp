47205e53464276164a40403bcf7e3f81cfdba3ae
Roman <roman.miv@yandex.ru>
2025-10-14 19:15:40 +0300

    [a] Add first analyse

--- include/analyse.hpp
+++ include/analyse.hpp
@@ -27,22 +27,35 @@ namespace analyser {
 namespace rv = std::ranges::views;
 namespace rs = std::ranges;
 
-auto AnalyseFunctions(const std::vector<std::string>& files,
-                      const analyser::metric::MetricExtractor& metric_extractor) {
+auto AnalyseFunctions(const std::vector<std::string> &files,
+                      const analyser::metric::MetricExtractor &metric_extractor) {
+    auto resRanges = files | std::views::transform([](auto &&str) {
+                         file::File f(str);
+                         return function::FunctionExtractor{}.Get(f);
+                     }) |
+                     std::views::join | std::views::transform([&metric_extractor](auto &&func) {
+                         auto res = metric_extractor.Get(func);
+                         return std::make_pair(func, res);
+                     });
+    std::vector<std::pair<function::Function, metric::MetricResults>> res;
+    res.reserve(std::ranges::distance(res));
+    for (auto ele : resRanges) {
+        res.emplace_back(std::move(ele));
+    }
+    return res;
+}
+
+auto SplitByClasses(const auto &analysis) {
     // здесь ваш код
 }
 
-auto SplitByClasses(const auto& analysis) {
+auto SplitByFiles(const auto &analysis) {
     // здесь ваш код
 }
 
-auto SplitByFiles(const auto& analysis) {
+void AccumulateFunctionAnalysis(const auto &analysis,
+                                const analyser::metric_accumulator::MetricsAccumulator &accumulator) {
     // здесь ваш код
 }
 
-void AccumulateFunctionAnalysis(
-    const auto& analysis, const analyser::metric_accumulator::MetricsAccumulator& accumulator) {
-    // здесь ваш код
-}
-
-} // namespace analyser
+}  // namespace analyser
