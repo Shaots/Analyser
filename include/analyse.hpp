915d2b0b29c3e2dfcd6c82a92fc75a569433e9eb
Roman <roman.miv@yandex.ru>
2025-10-15 18:00:12 +0300

    [a] Add all analyse

--- include/analyse.hpp
+++ include/analyse.hpp
@@ -46,16 +46,48 @@ auto AnalyseFunctions(const std::vector<std::string> &files,
 }
 
 auto SplitByClasses(const auto &analysis) {
-    // здесь ваш код
+    auto resRanges = analysis | std::views::filter([](const auto& p){
+        return p.first.class_name.has_value();
+    }) | std::views::chunk_by([](const auto& left, const auto& right){
+        return left.first.class_name.value() == right.first.class_name.value();
+    });
+    // | rs::to<std::vector<std::vector<std::pair<function::Function, metric::MetricResults>>>>;
+    
+    std::vector<std::vector<std::pair<function::Function, metric::MetricResults>>> res;
+
+    res.resize(std::ranges::distance(resRanges));
+    for (auto [index, vec] : std::views::zip(std::views::iota(0), resRanges)) {
+        res[index].reserve(std::ranges::distance(vec));
+        for (auto ele :vec) {
+            res[index].emplace_back(std::move(ele));
+        }
+    }
+    return res;
 }
 
 auto SplitByFiles(const auto &analysis) {
-    // здесь ваш код
+    auto resRanges = analysis | std::views::chunk_by([](const auto& left, const auto& right){
+        return left.first.filename == right.first.filename;
+    });
+    // | rs::to<std::vector<std::vector<std::pair<function::Function, metric::MetricResults>>>>;
+    std::vector<std::vector<std::pair<function::Function, metric::MetricResults>>> res;
+
+    res.resize(std::ranges::distance(resRanges));
+    for (auto [index, vec] : std::views::zip(std::views::iota(0), resRanges)) {
+        res[index].reserve(std::ranges::distance(vec));
+        for (auto ele :vec) {
+            res[index].emplace_back(std::move(ele));
+        }
+    }
+    return res;
+
 }
 
 void AccumulateFunctionAnalysis(const auto &analysis,
                                 const analyser::metric_accumulator::MetricsAccumulator &accumulator) {
-    // здесь ваш код
+    rs::for_each(analysis, [&accumulator](const auto &metric) {
+        accumulator.AccumulateNextFunctionResults(metric);
+    }, &std::pair<function::Function, metric::MetricResults>::second);
 }
 
 }  // namespace analyser
