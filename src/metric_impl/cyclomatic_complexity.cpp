1e1fc43aa88f47d74b2fefaf341d2fe508fcc501
Roman <roman.miv@yandex.ru>
2025-10-16 20:03:11 +0300

    [u] Update src/metric_impl/cyclomatic_complexity.cpp

--- src/metric_impl/cyclomatic_complexity.cpp
+++ src/metric_impl/cyclomatic_complexity.cpp
@@ -13,16 +13,20 @@
 #include <ranges>
 #include <sstream>
 #include <string>
+#include <unordered_set>
 #include <variant>
 #include <vector>
 
 namespace analyser::metric::metric_impl {
 
 MetricResult::ValueType CyclomaticComplexityMetric::CalculateImpl(const function::Function &f) const {
+    static const std::unordered_set<std::string> forbiddenKeys = {"return", "case_clause"};
+    static const std::unordered_set<std::string> necessaryKeys = {"statement", "clause", "case_pattern", "conditional_expression"};
     auto res = f.ast | std::views::split('\n') |
                std::views::transform([](auto &&str) { return std::string_view{str}; }) |
                std::views::filter([](auto &&str) {
-                   return !str.contains("return") && !str.contains("case_clause") && (str.contains("statement") || str.contains("clause") || str.contains("case_pattern") || str.contains("conditional_expression"));
+                   return std::ranges::none_of(forbiddenKeys, [&str](const auto &key){ return str.contains(key);}) &&
+                          std::ranges::any_of(necessaryKeys, [&str](const auto &key){ return str.contains(key);});
                });
     return (int)std::ranges::distance(res);
 }
